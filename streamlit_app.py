f95c078a730a050a60e2c0dca3aa5748f4fc1a88
jayashrimannur <61403207+jayashrimannur@users.noreply.github.com>
2025-07-08 20:24:28 +0530

    Update streamlit_app.py
    Using search_on to search in the database

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #Convert the Snowpark dataframe to pandas dataframe so that we can use LOC function
 #st.dataframe(data=my_dataframe, use_container_width=True)
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
+#st.dataframe(pd_df)
 #st.stop()
 ingredients_list = st.multiselect('Choose upto 5 ingredients:',my_dataframe,default=None,max_selections=5)
 #if ingredients_list:
@@ -37,9 +37,9 @@ if ingredients_list:
 for fruit_chosen in ingredients_list:
     ingredients_string+=fruit_chosen+' '
     search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-    st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+    #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
     st.subheader(fruit_chosen + ' Nutritional Information')
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
     sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 #st.write(ingredients_string)
 time_to_insert=st.button('Submit Order')
